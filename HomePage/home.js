65feb084081cc2ee6711714f86e8000842ad3110
rasikaG23 <rasikargujar23@gmail.com>
2023-02-02 01:45:41 +0530

    home

--- HomePage/home.js
+++ HomePage/home.js
@@ -1,3 +1,9 @@
+/*imageSliderContainer */
+const imageSlider = [
+    
+]
+
+/*** Best Of Electronics***/
 const electronicsProductData = [
   {
       img : "https://rukminim1.flixcart.com/image/400/400/kokdci80/dslr-camera/v/e/x/z-24-200mm-z5-nikon-original-imag2zuekuxgxsgg.jpeg?q=70",
@@ -53,7 +59,66 @@ const electronicsProductData = [
   },
 ]
 
+/*** Beauty, Food, Toys & more***/
 
+
+const beutyFoodToyData = [
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/kx50gi80/pen/h/z/k/119766-flair-original-imag9nzubznagufg.jpeg?q=70",
+        productName : "Top Selling Stationery",
+        discount : "From ₹49",
+        brand : "Pens, Notebooks & more",
+        
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/l4d2ljk0/bat/3/4/4/1000-hitman-full-size-double-blade-poplar-cricket-bat-advance-original-imagf9zzgxxfamce.jpeg?q=70",
+        productName : "Cricket, Football & More",
+        discount : "Up to 70% Off",
+        brand : "Ceat, Nivia, Li-Ning...",
+       
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/krtjgcw0/headphone/d/9/g/au-mh501-maono-original-imag5j35rffkwpac.jpeg?q=70",
+        productName : "Studio Headphones",
+        discount : "Up to 80% off",
+        brand : "Explore Now!",
+       
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/jlph9jk0/cycle/h/f/k/skyper-26t-sskp26bk0001-16-hero-original-imaf8ru5xysfgtmx.jpeg?q=70",
+        productName : "Premium Non-Geared Cycles",
+        discount : "Up to 40% Off",
+        brand : "Hercules, Hero & More",
+       
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/jxz0brk0/stuffed-toy/n/t/s/4-feet-pink-very-beautiful-best-quality-for-special-gift-125-13-original-imafgv92puzkdytg.jpeg?q=70",
+        productName : "Soft Toys",
+        discount : "Upto 70% Off",
+        brand : "Stuffed Toys, Plush Toys & more"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/k7w8eq80/two-wheeler-tyre/v/y/s/90-90-12-106061-milaze-tl-54j-sw-ceat-original-imafqyx5tnfraaxh.jpeg?q=70",
+        productName : "Tyres",
+        discount : "From 799",
+        brand : "Apollo, Bridgestone, Ceat and"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/k6fd47k0/nut-dry-fruit/p/z/7/200-100-natural-california-pouch-happilo-original-imafzvw2tcazeur6.jpeg?q=70",
+        productName : "Dry Fruits",
+        discount : "Upto 75% Off",
+        brand : "Happilo, Farmley & More"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/jk1grrk0/car-pressure-washer/j/c/m/06008a7af0-bosch-original-imaf7hd6bv8g3m6r.jpeg?q=70",
+        productName : "Car pressure washer",
+        discount : "Upto 65% Off",
+        brand : "Bosch, Black & Decker & More"
+    },
+  ]
+/*imageSliderContainer */
+
+/*** Best Of Electronics***/
 let bestofElctronic_product_itemEl = document.querySelector(".bestofElctronic_product_item")
 let bestofElectornicProduct_html = ""
 
@@ -99,3 +164,58 @@ cardBtnRightEl.addEventListener("click", function(){
     cardBtnRightEl.style.display = "block" 
     cardBtnLeftEl.style.display = "none" 
   })
+
+  /*** Beauty, Food, Toys & more ***/
+
+
+  let BeautyFoodToy_itemBFT = document.querySelector(".BeautyFoodToy_product_item")
+let BeautyFoodToy_html = ""
+
+console.log(beutyFoodToyData)
+beutyFoodToyData.forEach(el => {
+
+    
+    
+    BeautyFoodToy_html += `
+    <div >
+        <a href="${el.link}" class="BestofElectronic_item">   
+        <div class="bestOfElectornic_image_Product">
+            <img
+                src="${el.img}" />
+        </div>
+        <div class="bestofElectronicmoreOption">
+            <p class="bestofElectornicProduct_name">${el.productName}</p>
+            <p class="bestofElecronic_discount">${el.discount}</p>
+            <p class="bestofElectronic_brand">${el.brand}</p>
+        </div>
+        </a>
+    </div>
+    
+    `
+})
+
+BeautyFoodToy_itemBFT.innerHTML = BeautyFoodToy_html
+
+let cardBtnRightBFT = document.getElementById("card-btn-right") 
+let cardBtnLeftBFT = document.getElementById("card-btn-left") 
+cardBtnLeftBFT.style.display = "none" 
+
+
+
+
+cardBtnRightBFT.addEventListener("click", function(){
+    BeautyFoodToy_itemBFT.style.transform = "translateX(-65%)" 
+   cardBtnRightBFT.style.display = "none" 
+   cardBtnLeftBFT.style.display="block" 
+  }) 
+  
+  cardBtnLeftBFT.addEventListener("click", function(){ 
+    BeautyFoodToy_itemBFT.style.transform = "translatex(0%)" 
+    cardBtnRightBFT.style.display = "block" 
+    cardBtnLeftBFT.style.display = "none" 
+  })
+
+
+  
+
+  
